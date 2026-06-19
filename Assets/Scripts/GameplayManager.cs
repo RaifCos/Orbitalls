2382672a1466f190bf66a45929cd739e75d4197a
RaifCos <raifcostellobusiness@gmail.com>
2026-06-19 16:59:37 +0100

    Planet Spin Controls

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -4,21 +4,26 @@ using UnityEngine.InputSystem;
 public class GameplayManager : MonoBehaviour {
 
     private GameObject selectedPlanet;
+    [SerializeField] private InputAction planetSpin;
+    [SerializeField] private float planetSpinSpeed;
+    
     private void Awake() { GameManager.gameplayManager = this; }
+
+    private void OnEnable() => planetSpin.Enable();
+    private void OnDisable() => planetSpin.Disable();
 
     private void Update() {
         if (Mouse.current.leftButton.wasPressedThisFrame) {
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
             RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
 
-            if (hit.collider != null)
-                SelectPlanet(hit.collider.gameObject);
+            if (hit.collider != null) selectedPlanet = hit.collider.gameObject;
         }
-    }
 
-    private void FixedUpdate() {
-        if (selectedPlanet != null)
-            selectedPlanet.transform.Rotate(Vector3.forward, 20 * Time.fixedDeltaTime);
+        if (selectedPlanet != null) {
+            float spinInput = planetSpin.ReadValue<float>();
+            selectedPlanet.transform.Rotate(Vector3.forward, spinInput * planetSpinSpeed * Time.deltaTime);
+        }
     }
 
     public void SelectPlanet(GameObject planet) => selectedPlanet = planet;
