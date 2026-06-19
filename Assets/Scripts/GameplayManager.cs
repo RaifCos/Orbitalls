8b0bdd9716c03226f2e3ce1923865d0fa7204678
RaifCos <raifcostellobusiness@gmail.com>
2026-06-19 17:17:00 +0100

    Orbit Mechanic

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -3,26 +3,44 @@ using UnityEngine.InputSystem;
 
 public class GameplayManager : MonoBehaviour {
 
-    private GameObject selectedPlanet;
+    [Header("Player Inputs")]
     [SerializeField] private InputAction planetSpin;
+    [SerializeField] private InputAction planetOrbit;
+
+    [Header("Planet Speeds")]
     [SerializeField] private float planetSpinSpeed;
+    [SerializeField] private float planetOrbitSpeed;
+
+    [Header("Selected Planet")]
+    private GameObject selectedPlanet;
+    private Planet selectedPlanetComponent;
     
     private void Awake() { GameManager.gameplayManager = this; }
 
-    private void OnEnable() => planetSpin.Enable();
-    private void OnDisable() => planetSpin.Disable();
+    private void OnEnable()  { planetSpin.Enable();  planetOrbit.Enable();  }
+    private void OnDisable() { planetSpin.Disable(); planetOrbit.Disable(); }
 
     private void Update() {
         if (Mouse.current.leftButton.wasPressedThisFrame) {
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
             RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
 
-            if (hit.collider != null) selectedPlanet = hit.collider.gameObject;
+            if (hit.collider != null) {
+                selectedPlanet = hit.collider.gameObject;
+                selectedPlanetComponent = selectedPlanet.GetComponent<Planet>();
+            }
         }
 
         if (selectedPlanet != null) {
+            // Spin Planet around its own axis.
             float spinInput = planetSpin.ReadValue<float>();
             selectedPlanet.transform.Rotate(Vector3.forward, spinInput * planetSpinSpeed * Time.deltaTime);
+
+            // Orbit Planet around its parent (if applicable).
+            if (selectedPlanet.GetComponent<Planet>().OrbitsPlanet == true) {
+                float orbitInput = planetOrbit.ReadValue<float>();
+                selectedPlanetComponent.Orbit(orbitInput * planetOrbitSpeed, Time.deltaTime);
+            }
         }
     }
 
--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -2,4 +2,24 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour {
 
+    [SerializeField] private GameObject parentPlanet;
+    [SerializeField] private float orbitRadius;
+    private float orbitAngle;
+
+    public bool OrbitsPlanet => parentPlanet != null;
+
+    public void Orbit(float speed, float deltaTime) {
+        orbitAngle += speed * deltaTime;
+
+        if (orbitAngle >= 360f) orbitAngle -= 360f;
+
+        Vector2 parentPos = parentPlanet.transform.position;
+        float rad = orbitAngle * Mathf.Deg2Rad;
+
+        transform.position = new Vector2(
+            parentPos.x + orbitRadius * Mathf.Cos(rad),
+            parentPos.y + orbitRadius * Mathf.Sin(rad)
+        );
+    }
+    
 }
