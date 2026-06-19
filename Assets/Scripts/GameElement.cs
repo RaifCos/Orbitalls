a1d0013c42f35796096d5dcbc311d41adedf0498
RaifCos <raifcostellobusiness@gmail.com>
2026-06-19 22:11:37 +0100

    Switch to Raycast System

--- Assets/Scripts/GameElement.cs
+++ Assets/Scripts/GameElement.cs
@@ -1,33 +1,107 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class GameElement : MonoBehaviour {
 
-    public void SetElement(Element element, int direction, int reach) {
+    private int direction;
+    private int reach;
+    private Element element;
+
+    [Header("Raycast Detection")]
+    private GamePlanet currenRaytHit;
+    private List<GamePlanet> currentRadialHits = new();
+
+
+    public void SetElement(Element e, int d, int r) {
+        element = e;
+        direction = d;
+        reach = r;
         gameObject.name = element.internalName;
 
         switch (direction) {
             case 1:
                 gameObject.GetComponent<SpriteRenderer>().sprite = element.raySprite;
-                gameObject.GetComponent<BoxCollider2D>().enabled = true;
                 transform.localScale = new Vector3(1f, reach, 1f);
                 break;
             case 2:
                 gameObject.GetComponent<SpriteRenderer>().sprite = element.radialSprite;
-                gameObject.GetComponent<CircleCollider2D>().enabled = true;
                 transform.localScale = new Vector3(reach * 2f, reach * 2f, 1f);
                 break;
             default:
                 transform.localScale = Vector3.one;
                 break;
         }
-
     }
 
-    public void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.gameObject.CompareTag("Planet")) {
-            if (collision.gameObject.TryGetComponent<GamePlanet>(out var otherPlanet)) {
-                Debug.Log($"Element {gameObject.name} collided with Planet {otherPlanet.name}");
-            }
+    public void Detect() {
+        int planetLayer = LayerMask.GetMask("Planet");
+        switch (direction) {
+            case 1:
+                RayDetect(planetLayer);
+                break;
+            case 2:
+                RadialDetect(planetLayer);
+                break;
         }
+    }
+
+    
+    private void RayDetect(int planetLayer) {
+        Vector2 origin = transform.parent.position;
+        Vector2 rayDir = transform.parent.up;
+
+        float parentRadius = transform.parent.GetComponent<CircleCollider2D>().radius;
+        Vector2 offsetOrigin = origin + rayDir * parentRadius;
+        float adjustedReach = reach - parentRadius;
+
+        RaycastHit2D hit = Physics2D.Raycast(offsetOrigin, rayDir, adjustedReach, planetLayer);
+        GamePlanet hitPlanet = null;
+
+        if (hit.collider != null
+            && hit.collider.gameObject != transform.parent.gameObject
+            && hit.collider.TryGetComponent<GamePlanet>(out var found)) {
+            hitPlanet = found;
+        }
+
+        if (hitPlanet != null && hitPlanet != currenRaytHit) ElementHit(hitPlanet);
+
+        currenRaytHit = hitPlanet;
+    }
+
+
+    private void RadialDetect(int planetLayer) {
+        Vector2 origin = transform.parent.position;
+        float parentRadius = transform.parent.GetComponent<CircleCollider2D>().radius;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(origin, reach, planetLayer);
+
+        List<GamePlanet> frameHits = new();
+        foreach (var collider in colliders) {
+            if (collider.gameObject == transform.parent.gameObject) continue;
+            if (!collider.TryGetComponent<GamePlanet>(out var hitPlanet)) continue;
+
+            Vector2 targetPos = collider.transform.position;
+            Vector2 toTarget = targetPos - origin;
+            Vector2 dir = toTarget.normalized;
+            float distance = toTarget.magnitude;
+
+            Vector2 offsetOrigin = origin + dir * parentRadius;
+            float adjustedDistance = distance - parentRadius;
+
+            RaycastHit2D hit = Physics2D.Raycast(offsetOrigin, dir, adjustedDistance, planetLayer);
+
+            if (hit.collider != null && hit.collider.gameObject == collider.gameObject) frameHits.Add(hitPlanet);
+        }
+
+        foreach (var planet in frameHits) {
+            if (!currentRadialHits.Contains(planet)) ElementHit(planet);
+        }
+
+        currentRadialHits = frameHits;
+    }
+
+    private void ElementHit(GamePlanet target) {
+        Debug.Log($"Element {element.internalName} hit planet {target.name}");
+        //TODO: Add Logic for when an element hits a planet (planet state should change)
     }
 }
--- Assets/Scripts/GamePlanet.cs
+++ Assets/Scripts/GamePlanet.cs
@@ -10,26 +10,21 @@ public class GamePlanet : MonoBehaviour {
     [SerializeField] private float orbitRadius;
     private float orbitAngle;
 
+    private GameElement gameElement;
+
     public bool OrbitsPlanet => parentPlanet != null;
 
     void Awake() {
         gameObject.name = planetType.externalName;
         gameObject.GetComponent<SpriteRenderer>().sprite = planetType.sprite;
         gameObject.GetComponent<CircleCollider2D>().radius = planetType.radius;
+        
+        GameObject elementGO = transform.GetChild(0).gameObject;
         if (planetType.elementType != null) {
-            GameObject elementGO = transform.GetChild(0).gameObject;
-            if (planetType.outputDirection == 0) {
-                elementGO.SetActive(false);
-            } else {
-                if (planetType.outputDirection == 1) elementGO.transform.localPosition = new Vector3(0f, 0.5f + planetType.radius, 0f);
-                elementGO.GetComponent<GameElement>().SetElement(planetType.elementType, planetType.outputDirection, outputReach);
-            }
-        }
-    }
-
-    public void DriveOrbit(float speed, float deltaTime) {
-        orbitAngle += speed * deltaTime;
-        if (orbitAngle >= 360f) orbitAngle -= 360f;
+            if (planetType.outputDirection == 1) { elementGO.transform.localPosition = new Vector3(0f, 2f * planetType.radius, 0f); }
+            gameElement = elementGO.GetComponent<GameElement>();
+            gameElement.SetElement(planetType.elementType, planetType.outputDirection, outputReach);
+        } else elementGO.SetActive(false);
     }
 
     private void Update() {
@@ -38,9 +33,21 @@ public class GamePlanet : MonoBehaviour {
         Vector2 parentPos = parentPlanet.transform.position;
         float rad = orbitAngle * Mathf.Deg2Rad;
 
-        transform.position = new Vector2(
+        transform.position = new Vector2 (
             parentPos.x + orbitRadius * Mathf.Cos(rad),
             parentPos.y + orbitRadius * Mathf.Sin(rad)
         );
+    }
+
+    private void LateUpdate() {
+        if (gameElement == null) return;
+        gameElement.Detect();
+    }
+
+    public void ApplySpin(float degrees) => transform.Rotate(Vector3.forward, degrees);
+
+    public void DriveOrbit(float speed, float deltaTime) {
+        orbitAngle += speed * deltaTime;
+        if (orbitAngle >= 360f) orbitAngle -= 360f;
     }
 }
